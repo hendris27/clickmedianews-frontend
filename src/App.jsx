7ff0aee5a2a5aa4ba2c2a609c6ce5326be512a3b
fahmipraga <itmamul2004@gmail.com>
2023-05-25 13:59:58 +0700

    fix: daisyui and forgot password

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import {  BrowserRouter, Routes, Route} from "react-router-dom"
 
 import Home from "./pages/users/Home"
+import ForgotPassword from "./pages/users/auth/forgot-password"
 
 
 // import { Provider } from "react-redux"
@@ -16,6 +17,7 @@ const App = () => {
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Home/>} />
+                <Route path='/forgot-password' element={<ForgotPassword/>} />
             </Routes>
         </BrowserRouter>
     
