8b66520ca050da6071f9b9661a7f7d863c29c8eb
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-05-25 14:25:46 +0700

    add:router file

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import {  BrowserRouter, Routes, Route} from "react-router-dom"
 
 import Home from "./pages/users/Home"
+import SearchArticles from "./pages/users/SearchArticles"
+import Articles from "./pages/users/Articles"
+import ForgotPassword from "./pages/users/auth/ForgotPassword"
+import SignIn from "./pages/users/auth/SignIn"
+import SignUp from "./pages/users/auth/SignUp"
 
 
 // import { Provider } from "react-redux"
@@ -16,6 +21,21 @@ const App = () => {
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Home/>} />
+            </Routes>
+            <Routes>
+                <Route path='/searcharticles' element={<SearchArticles/>} />
+            </Routes>
+            <Routes>
+                <Route path='/articles' element={<Articles/>} />
+            </Routes>
+            <Routes>
+                <Route path='/forgotpassword' element={<ForgotPassword/>} />
+            </Routes>
+            <Routes>
+                <Route path='/signin' element={<SignIn/>} />
+            </Routes>
+            <Routes>
+                <Route path='/signup' element={<SignUp/>} />
             </Routes>
         </BrowserRouter>
     
