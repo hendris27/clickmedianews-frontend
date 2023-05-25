e9824f3443f283bf99fc60da1b4b0ae4fc601529
hendri27 <88545100+hendris27@users.noreply.github.com>
2023-05-25 23:10:21 +0700

    fix signup

--- src/pages/users/auth/SignUp.jsx
+++ src/pages/users/auth/SignUp.jsx
@@ -21,7 +21,77 @@ function SignUp() {
         <Fragment>
             <div className='flex w-full h-full'>
                 <div className="hidden md:flex flex-1 bg-[url('/src/assets/img/picture_login.png')] bg-cover bg-bottom bg-no-repeat"></div>
-<div>1<div>
+                <div className='flex flex-col flex-1 bg-white gap-10 pt-10 pb-10 md:h-full'>
+                    <div className='flex items-center flex-col justify-center gap-3'>
+                        <Formik initialValues={{email: "", password: "", phoneNumber: ""}} onSubmit={doSignUp} validationSchema={validationSchema}>
+                            {({values, errors, touched, handleBlur, handleSubmit, handleChange, isSubmitting}) => {
+                                return (
+                                    <form onSubmit={handleSubmit} className='flex flex-col gap-3'>
+                                        <div className='text-[36px] font-bold'>Sign Up</div>
+                                        <div>
+                                            <label htmlFor='email' className='font-bold'>Email :</label>
+                                            <input type='email' id='email' name='email' placeholder='Enter your email address' className='input input-bordered w-full max-w-xs mt-2 mb-2' onChange={handleChange} onBlur={handleBlur} value={values.email}/>
+                                            {errors.email &&
+                                                touched.email && (
+                                                <label
+                                                    htmlFor='email'
+                                                    className='label p-0'
+                                                >
+                                                    <span className='label-text-alt font-bold text-md text-error'>
+                                                        {errors.email}
+                                                    </span>
+                                                </label>
+                                            )}
+                                        </div>
+                                        <div>
+                                            <label htmlFor='password' className='font-bold'>Password</label>
+                                            <input type='password' id='password' name='password' placeholder='Enter your password' className='input input-bordered w-full max-w-xs mt-2 mb-2' onChange={handleChange} onBlur={handleBlur} value={values.password}/>
+                                            {errors.password &&
+                                                touched.password && (
+                                                <label
+                                                    htmlFor='password'
+                                                    className='label p-0'
+                                                >
+                                                    <span className='label-text-alt font-bold text-md text-error'>
+                                                        {errors.password}
+                                                    </span>
+                                                </label>
+                                            )}
+                                        </div>
+                                        <div>
+                                            <label htmlFor='phoneNumber' className='font-bold'>Phone Number</label>
+                                            <input type='text' placeholder='Enter your phone number' id='phoneNumber' name='phoneNumber' className='input input-bordered w-full max-w-xs mt-2 mb-2' onChange={handleChange} onBlur={handleBlur} value={values.phoneNumber}/>
+                                            {errors.phoneNumber &&
+                                                touched.email && (
+                                                <label
+                                                    htmlFor='phoneNumber'
+                                                    className='label p-0'
+                                                >
+                                                    <span className='label-text-alt font-bold text-md text-error'>
+                                                        {errors.phoneNumber}
+                                                    </span>
+                                                </label>
+                                            )}
+                                        </div>
+                                        <div>
+                                            <button disabled={isSubmitting} className='btn bg-primary text-white border-0 w-full normal-case'>Sign Up</button>
+                                        </div>
+                                    </form>
+                                )
+                            }}
+                        </Formik>
+                        <div className='flex flex-col items-center gap-3 w-[43%]'>
+                            <p className='uppercase font-bold'>Or sign up with</p>
+                            <div className='flex gap-5'>
+                                <div><FcGoogle size={35} color='3366ff'/></div>
+                                <div><FaFacebook size={35} color='#3366ff'/></div>
+                                <div><FaTwitter size={35} color='#3366ff'/></div>
+                            </div>
+                            <div className='font-bold'>
+                                Already have an account ?
+                            </div>
+                            <div className='w-full'>
+                                <button className='btn text-white border-0 w-full normal-case'>Login Here</button>
                             </div>
                             <div className='font-bold'><Link>Back to homepage</Link></div>
                         </div>
