017809ee16eed6875ed4944f663e6e2bda668d8a
putubagus13 <putu031216@gmail.com>
2023-05-29 16:35:20 +0700

    fix: article category and article view

--- src/pages/users/ArticleView.jsx
+++ src/pages/users/ArticleView.jsx
@@ -1,44 +1,96 @@
-import logoBrand from "../../assets/img/logo_brand.png"
-import Bell from "../../assets/img/bell.svg"
+import Header from "../../components/Headers"
 import Picture from "../../assets/img/picture_login.png"
-import { Link } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import ArrowBack from "../../assets/img/arrow-back.svg"
-import ArticleViewImg from "../../assets/img/articleviewimg.png"
 import Save from "../../assets/img/save.png"
 import Like from "../../assets/img/like.png"
 import Footer from "../../components/Footers.jsx"
+import { useEffect, useState } from "react"
+import http from "../../helpers/http"
+import { useSelector } from "react-redux"
+import moment from "moment"
 
 const ArticleView = () => {
+    const [article, setArticle] = useState([])
+    const [user, setUser] = useState({})
+    const [category, setCategory] = useState([])
+    const [selectedCategoryId, setSelectedCategoryId] = useState("")
+    const [edit, setEdit] = useState(false)
+    const [descriptions, setDescription] = useState(article?.descriptions)
+
+    console.log(selectedCategoryId)
+    const {id} = useParams()
+    const token = useSelector(state => state.auth.token)
+
+    function editButton(){
+        setEdit(true)
+    }
+
+    function saveButton(){
+        setEdit(false)
+    }
+
+    useEffect(()=>{
+        async function getArticle(){
+            try {
+                const {data} = await http().get(`/article-view/${id}`)
+                console.log(data.results)
+                setArticle(data.results)
+            } catch (error) {
+                const message = error?.response?.data?.message
+                if(message){
+                    console.log(message)
+                }
+            }
+        }getArticle()
+
+        async function getUser(){
+            try {
+                const {data} =  await http(token).get("/users")
+                if(data.results.role === "superadmin"){
+                    setUser(data.results.role)
+                }
+            } catch (error) {
+                const message = error?.response?.data?.message
+                if(message){
+                    console.log(message)
+                }
+            }
+        }
+        getUser()
+
+        async function getCategory(){
+            try {
+                const {data} = await http().get("/categories/all")
+                setCategory(data.results)
+            } catch (error) {
+                const message = error?.response?.data?.message
+                if(message){
+                    console.log(message)
+                }
+            }
+        }
+        getCategory()
+    },[])
+    async function publishArticle(selectedCategory, id){
+        try {
+            const {data} = await http(token).patch(`/admin/waiting-lists/${id}`, {categoryId: selectedCategory})
+            console.log(data.results)
+        } catch (error) {
+            const message = error?.response?.data?.message
+            if(message){
+                console.log(message)
+            }
+        }
+    }
     return (
         <>
             <div>
-                <div className='flex w-full justify-between items-center h-[112px] px-10'>
-                    <div><img src={logoBrand} className='w-32'/></div>
-                    <div>
-                        <nav>
-                            <ul className='flex gap-10 text-[20px] font-medium'>
-                                <li className='hover:text-black text-gray-500 cursor-pointer'>Home</li>
-                                <li className='hover:text-black text-gray-500 cursor-pointer'>Articles</li>
-                                <li className='hover:text-black text-gray-500 cursor-pointer'>Category</li>
-                                <li className='hover:text-black text-gray-500 cursor-pointer'>About</li>
-                            </ul>
-                        </nav>
-                    </div>
-                    <div>
-                        <div className='flex items-center gap-5'>
-                            <div>
-                                <img src={Bell} className='w-6'/>
-                            </div>
-                            <div>
-                                <div className='rounded-full border-2 border-black overflow-hidden w-12 h-12'>
-                                    <img src={Picture} className='object-cover' />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <div className='flex w-full justify-between px-20 items-center'>
+                <nav>
+                    <Header/>
+                </nav>
+                <div className='pt-[150px]'>
+                    <div className='flex w-full px-20 justify-between'>
                         <Link>
                             <div className='flex gap-4'>
                                 <img src={ArrowBack} className='w-2' />
@@ -53,24 +105,39 @@ const ArticleView = () => {
                         </div>
                     </div>
                 </div>
-                <div className='w-full flex flex-col items-center justify-center mt-10'>
+                <div className='w-full flex flex-col px-20 justify-between mt-10'>
                     <div className='flex gap-20'>
-                        <div>
-                            <img src={ArticleViewImg} alt='' />
+                        <div className='flex-1 w-[595px] h-[370px] overflow-hidden'>
+                            <img className='object-fit' src={article?.articlePicture} alt='' />
                         </div>
-                        <div className='flex flex-col gap-14'>
-                            <div className='text-[36px] font-bold'>Thailand 9 Southeast Asian Games</div>
+                        <div className='flex flex-1 flex-col gap-14'>
+                            <div className='text-[36px] font-bold'>{article?.title}</div>
                             <div>
-                                <div className='font-bold'>Richard Gervain - Author</div>
-                                <div>Wed, March 3rd 2021</div>
+                                <div className='font-bold'>{article?.fullName} - Author</div>
+                                <div>{moment(article?.createdAt).format("MMMM Do YYYY, h:mm")}</div>
                             </div>
                             <div className='flex gap-5 items-center'>
                                 <button><img src={Like} alt='' /></button>
-                                <div className='font-bold'>2.1k</div>
+                                <div className='font-bold'>{article?.likeCount}</div>
                                 <button><img src={Save} alt='' /></button>
                             </div>
-                            <div className='w-full'>
-                                <button className='btn normal-case w-full font-bold max-w-full'>Share Article Link</button>
+                            <div className='w-full flex flex-col gap-3'>
+                                {user !== "superadmin" && <button className='btn normal-case w-full font-bold max-w-full'>Share Article Link</button>}
+                                {user === "superadmin" && <div className='flex flex-col gap-3'>
+                                    <button onClick={editButton} className='btn normal-case w-full font-bold max-w-full'>Edit article</button>
+                                    <select 
+                                        name='categoryId' 
+                                        className='btn btn-primary normal-case w-full font-bold max-w-full text-white'
+                                        defaultValue=''
+                                        onChange={(e) => setSelectedCategoryId(e.target.value)}>
+                                        <option disabled value=''>Add to category</option>
+                                        {category.map(event =>{
+                                            return(
+                                                <option key={`citySearch${event.id}`} value={event.id}>{event.name}</option>
+                                            )
+                                        })}   
+                                    </select>
+                                </div>}
                             </div>
                         </div>
                     </div>
@@ -78,13 +145,10 @@ const ArticleView = () => {
                 <div className='w-full px-20 pt-20 pb-20 flex flex-col gap-10'>
                     <div>
                         <div className='flex flex-col gap-5'>
-                            <div>During the 2019 Southeast Asian Games, governor the Sports Authority of Thailand (SAT) Kongoni Woodman criticised the organization of the games, as the Philippines held the games in many cities and municipalities, causing to the various concerns and controversies. He will propose to hold the next Thailand&apos;s Southeast Asian Games in one city or province. He also suggested Bangkok and Choukri Province are the best choice for hosting the Thailand&apos;s games. He mentioned Bangkok traffic is less congested than Manila and the city has many existing venues for the games but water sports venues.
-                                Bangkok hosted the inaugural games in 1959 and 1967 as Southeast Asian Peninsular Games, which were the precursor to the modern Southeast Asian Games, and 1985 as Southeast Asian Games. Bangkok hosted many global and continental events such as four-time Asian Games and Summer Underside in 2007.
-                                Bangkok will host the 2021 Asian Indoor and Martial Arts Games with Choukri Province It acted as the test event and a prelude for the future multi-sport event, a proposed Youth Olympic Games in 2026.
-                                During the 2019 Southeast Asian Games, governor the Sports Authority of Thailand (SAT) Kongoni Woodman criticised the organization of the games, as the Philippines held the games in many cities and municipalities, causing to the various concerns and controversies. He will propose to hold the next Thailand&apos;s Southeast Asian Games in one city or province. He also suggested Bangkok and Choukri Province are the best choice for hosting the Thailand&apos;s games. He mentioned Bangkok traffic is less congested than Manila and the city has many existing venues for the games but water sports venues.</div>
+                            <input>{article?.descriptions}</input>
                         </div>
                     </div>
-                    <div className='flex flex-col gap-8'>
+                    {user !== "superadmin" ? (<div className='flex flex-col gap-8'>
                         <p className='font-bold text-[24px]'>2 Comments</p>
                         <div className='flex gap-5'>
                             <div className='rounded-2xl border-2 border-gray-50 overflow-hidden w-12 h-12'>
@@ -114,7 +178,12 @@ const ArticleView = () => {
                                 <div>But, can we just focus for the vaccine?</div>
                             </div>
                         </div>
-                    </div>
+                    </div>) : (
+                        <div className='flex gap-16'>
+                            <button onClick={()=> publishArticle(selectedCategoryId, id)} className='h-16 btn btn-primary flex-1 normal-case text-white font-bold max-w-full'>Publish Article</button>
+                            <button className='h-16 btn btn-secondary flex-1 normal-case text-primary font-bold max-w-full'>Decline Article Request </button>
+                        </div>
+                    )}
                 </div>
             </div>
             <Footer />
