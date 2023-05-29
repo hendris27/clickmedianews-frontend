072307c9f6d1718129088da4791e631e9d301921
DausHamyR <daus.amat354@gmail.com>
2023-05-29 14:57:43 +0700

    integrasi saved article

--- src/pages/users/SavedArticle.jsx
+++ src/pages/users/SavedArticle.jsx
@@ -1,20 +1,36 @@
 import Header from "../../components/Headers.jsx"
 import Footer from "../../components/Footers.jsx"
 import { IoIosArrowForward } from "react-icons/io"
-import { Link } from "react-router-dom"
-import Picture from "../../assets/img/picture_login.png"
-import CategoryImage from "../../assets/img/category-image-3.png"
+import { Link, useNavigate } from "react-router-dom"
+// import Picture from "../../assets/img/picture_login.png"
+// import CategoryImage from "../../assets/img/category-image-3.png"
 import { AiOutlineLike } from "react-icons/ai"
 import { BiTime } from "react-icons/bi"
 import Save from "../../assets/img/save.png"
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 import { useState, useEffect } from "react"
 import http from "../../helpers/http"
 import moment from "moment/moment.js"
+import { logout as logoutAction } from "../../redux/reducers/auth.js"
+import defaultPicture from "../../assets/img/default.jpg"
 
 function SavedArticle(){
     const [savePost, setSavePost] = useState([])
     const token = useSelector(state => state.auth.token)
+    const [profile, setProfile] = useState([])
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    
+    async function deleteSavePost(id){
+        try {
+            await http(token).delete(`/saved-article/${id}`)
+            setSavePost(savePost.filter(post => post.id !== id))
+        }catch(err) {
+            console.log(err)
+        }
+    }
+    deleteSavePost()
 
     useEffect(() => {
         async function getSavePost(){
@@ -26,7 +42,23 @@ function SavedArticle(){
             }
         }
         getSavePost()
-    }, [token])
+        async function getProfile(){
+            const { data } = await http(token).get("/profile")
+            setProfile(data.results)
+        }
+
+        getProfile()
+    }, [token, savePost])
+
+    const handleDelete = (id) => {
+        deleteSavePost(id)
+    }
+
+    const doLogout = ()=> {
+        window.localStorage.removeItem("token")
+        dispatch(logoutAction())
+        navigate("/signin")
+    }
     return (
         <>
             <div>
@@ -43,18 +75,20 @@ function SavedArticle(){
                                     <div className='flex gap-5 items-center w-full h-[120px] px-10'>
                                         <div className='border-2 rounded-3xl border-blue-500 p-1'>
                                             <div className='rounded-3xl border-2 border-gray-50 overflow-hidden w-16 h-16'>
-                                                <img src={Picture} className='object-cover' />
+                                                {profile.picture === null ? (
+                                                    <img src={defaultPicture} className='object-cover' />
+                                                ) : <img src={profile.picture} className='object-cover' />}
                                             </div>
                                         </div>
                                         <div className='flex flex-col flex-1'>
-                                            <div>@jonathan</div>
-                                            <div className='font-bold'>Joe Daniel</div>
+                                            <div>{profile.username}</div>
+                                            <div className='font-bold'>{profile.fullName}</div>
                                             <div>Member</div>
                                         </div>
                                     </div>
                                     <div className='px-10'>
                                         <div className='font-bold'>About Me</div>
-                                        <div>Madison Blackstone is a director of publisher, with experience managing global teams.</div>
+                                        <div>{profile.about}</div>
                                     </div>
                                     <div className='w-[255px] rounded-2xl absolute bottom-[-20px] right-5 h-[50px] bg-blue-500 pr-5 flex justify-between text-white text-center'>
                                         <div className='rounded-r-2xl border-1 rounded-l-2xl w-[70px] justify-center bg-blue-600'>
@@ -72,9 +106,9 @@ function SavedArticle(){
                                     </div>
                                 </div>
                             </div>
-                            <div className='w-full flex justify-center'><Link className='text-blue-500'>See Profile</Link></div>
+                            <div className='w-full flex justify-center'><Link to='/profile' className='text-blue-500'>See Profile</Link></div>
                             <div className='flex flex-col gap-5 '>
-                                <Link className='flex w-full justify-between items-center border-2 p-3 rounded-2xl hover:bg-blue-100 hover:text-blue-600'>
+                                <Link to='/edit-profile' className='flex w-full justify-between items-center border-2 p-3 rounded-2xl hover:bg-blue-100 hover:text-blue-600'>
                                     <div className='font-bold'>Edit Profile</div>
                                     <div><IoIosArrowForward/></div>
                                 </Link>
@@ -90,9 +124,9 @@ function SavedArticle(){
                                     <div className='font-bold'>Help</div>
                                     <div><IoIosArrowForward/></div>
                                 </Link>
-                                <Link className='flex justify-center items-center border-none p-3 rounded-2xl bg-blue-600 text-white'>
+                                <button onClick={doLogout} className='flex justify-center items-center border-none p-3 rounded-2xl bg-blue-600 text-white'>
                                     <div className='font-bold'>Logout</div>
-                                </Link>
+                                </button>
                             </div>
                         </div>
                         <div className='flex flex-col flex-1 items-center mt-20 gap-10'>
@@ -113,11 +147,11 @@ function SavedArticle(){
                                                         </div>
                                                         <div className='flex'>
                                                             <BiTime size={25}/>
-                                                            <p>{moment(article.createdAt).format("DD-MM-YYYY")}</p>
+                                                            <p>{moment(article.createdAt).fromNow("mm")} ago</p>
                                                         </div>
-                                                        <div>
+                                                        <button onClick={() => handleDelete(article.id)}>
                                                             <img src={Save} className='w-4' alt='' />
-                                                        </div>
+                                                        </button>
                                                     </div>
                                                 </div>
                                             </div>
