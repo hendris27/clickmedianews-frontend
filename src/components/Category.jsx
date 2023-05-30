0e1d9a2d613bb027a1dc584a6f094bf88c974fd1
DausHamyR <daus.amat354@gmail.com>
2023-05-30 15:47:41 +0700

    solve category

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -5,7 +5,7 @@ const Category = () => {
     const [category, setCategory] = useState([])
     useEffect(()=> {
         async function getCategory(){
-            const {data} = await http().get("/categories")
+            const {data} = await http().get("/categories/all")
             setCategory(data.results)
         }
         getCategory()
@@ -14,7 +14,7 @@ const Category = () => {
         <div className='flex flex-col gap-8 pt-[20px]'>
             <div className='flex justify-between font-semibold'>
                 <div>Category</div>
-                <div className='text-[#19A7CE]'>More</div>
+                <div className='text-[#444cd4] cursor-pointer'>More</div>
             </div>
             <div className='flex gap-8'>
                 {category.map(category => {
