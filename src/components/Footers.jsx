26de3c3ddff42b9d1f94ac6c31e6fd02412de1aa
fahmipraga <itmamul2004@gmail.com>
2023-06-04 10:57:25 +0700

    fix: fix footer

--- src/components/Footers.jsx
+++ src/components/Footers.jsx
@@ -4,22 +4,22 @@ import logoBrand from "../assets/img/logo_brand.png"
 const Footers = () => {
     return (
         <>
-            <div className='bg-secondary px-[145px] py-[150px] '>
-                <div className='flex justify-between text-medium'>
-                    <div className='flex flex-col gap-7 text-[24px] font-medium text-black'>
+            <div className='bg-secondary'>
+                <div className='flex justify-around gap-96 py-10'>
+                    <div className='flex flex-col gap-5'>
                         <div>Why News Today</div>
                         <div>Be an author</div>
                         <div>Community</div>
                         <div>FAQ</div>
                     </div>
-                    <div className='flex flex-col gap-7 font-medium text-black'>
-                        <div className='font-bold text-[36px] w-[190px] flex justify-center items-center ' >
+                    <div className='flex flex-col font-medium'>
+                        <div className='font-bold w-[150px] flex justify-center items-center' >
                             <img  src={logoBrand} alt='logo_footers' />
                         </div>
-                        <div className='text-[24px] '>Jendral Sudirman Street No. 23 <br/>
+                        <div>Jendral Sudirman Street No. 23 <br/>
                                 Jakarta, Indonesia</div>
-                        <div className='text-[24px] '>(621)989898934</div>
-                        <div className='text-[24px] '>newstoday@mail.com</div>
+                        <div>(621)989898934</div>
+                        <div>newstoday@mail.com</div>
                     </div>
                 </div>
             </div>
