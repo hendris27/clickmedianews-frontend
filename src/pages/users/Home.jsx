1f4a59219c24efcb66e4ca2ffbcbfb00f2775047
fahmipraga <itmamul2004@gmail.com>
2023-07-30 04:46:25 +0700

    fix: responsive home

--- src/pages/users/Home.jsx
+++ src/pages/users/Home.jsx
@@ -1,7 +1,6 @@
 import Header from '../../components/Headers';
 import Footer from '../../components/Footers';
 import ScrollToTop from '../../components/ScrollToTop';
-import banner from '../../assets/img/picture_main.jpg';
 import videoImg from '../../assets/img/Video.png';
 import image from '../../assets/img/Image.png';
 import card from '../../assets/img/Card.png';
@@ -39,12 +38,15 @@ const Article = ({ id, picture, title, descriptions, likeCount, createdAt }) => 
   }
 
   return (
-    <div key={id} className="flex bg-white w-[416px] rounded-3xl gap-8 drop-shadow-2xl ">
-      <div className="flex justify-between items-center">
-        <Link to={`/articleView/${id}`} className="w-[126px] h-[222px] rounded-3xl overflow-hidden bg-green-400">
+    <div
+      key={id}
+      className="flex bg-white w-[416px] rounded-3xl gap-8 drop-shadow-2xl hover:scale-[1.05] cursor-pointer duration-200"
+    >
+      <div className="flex justify-between items-center flex-col lg:flex-row gap-10 py-10">
+        <Link to={`/articleView/${id}`} className="w-full h-[222px] rounded-3xl overflow-hidden bg-green-400">
           <img src={picture} className="w-[100%] h-full object-cover" alt="" />
         </Link>
-        <div className="flex-1 pl-8">
+        <div className="flex px-5">
           <div className="flex flex-col gap-8">
             <Link to={`/articleView/${id}`} className="flex flex-col gap-4">
               <div className="text-[#444cd4] text-[20px] leading-[20px] ">{title}</div>
@@ -117,30 +119,31 @@ const Home = () => {
           <Header />
         </nav>
         <main>
-          <div className="w-full relative ">
-            <div className="relative">
-              <img src={banner} className="w-full" />
-            </div>
-            <div className="absolute top-0 left-0 flex flex-col  md:flex md:flex-row  bg-no-repeat bg-cover ">
-              <div className="flex flex-col h-full  gap-12 pl-[136px] pt-[100px] pb-[70px] ">
-                <div className="flex flex-col">
-                  <div className="text-[65px] md:w-[668px] lg:w-[800px] leading-[74px] ">Share Information</div>
-                  <div className="text-[65px] md:w-[668px] lg:w-[800px] leading-[74px] ">and Educate People</div>
-                </div>
-                <div className="text-[20px] w-[608px] leading-[32px] ">
-                  Everyone has their point of view of something, but just don’t be afraid to express the facts. Be an
-                  author and share you prespective of something to the world.
+          <div className="w-full">
+            <div className="flex flex-col w-full">
+              <div className="flex flex-col h-full">
+                <div className="flex flex-col w-full pt-20 h-[700px] bg-[url('/src/assets/img/bg-main.jpg')] bg-cover bg-no-repeat">
+                  <div className="flex items-center h-full px-8 md:px-20">
+                    <div className="max-w-3xl text-white">
+                      <div className="text-5xl md:text-[65px] leading-[74px]">Share Information</div>
+                      <div className="text-5xl md:text-[65px] leading-[74px]">and Educate People</div>
+                      <div className="md:text-[20px] leading-[32px]">
+                        Everyone has their point of view of something, but just don&apos;t be afraid to express the
+                        facts. Be an author and share you prespective of something to the world.
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="flex flex-col bg-white px-[60px] pt-[60px] pb-[101px]  ">
+          <div className="flex flex-col bg-white">
             <div className="flex flex-col gap-8">
               <div className="flex justify-between font-semibold">
                 <div>Popular</div>
                 <div className="text-[#444cd4] cursor-pointer">More</div>
               </div>
-              <div className="flex gap-16 font-semibold text-[#444cd4] cursor-pointer ">
+              <div className="flex gap-16 font-semibold text-[#444cd4] cursor-pointer flex-wrap">
                 <div className="hover:text-black">#ladygaga</div>
                 <div className="hover:text-black">#jokowidodo</div>
                 <div className="hover:text-black">#dayniki</div>
@@ -157,41 +160,47 @@ const Home = () => {
                     <div className="text-[#444cd4] cursor-pointer">More</div>
                   </Link>
                 </div>
-                <div className="">
-                  <div className="grid grid-cols-4 drop-shadow-3xl gap-y-12">
-                    {category.map((category) => {
-                      return (
-                        <div key={`category-${category.id}`}>
-                          <div className="flex flex-col items-center gap-4 drop-shadow-2xl">
-                            <div
-                              onClick={() => {
-                                getCategories(category.name);
-                              }}
-                              className="w-[202px] h-[222px] rounded-3xl overflow-hidden relative cursor-pointer"
-                            >
-                              <img src={category.picture} className="w-full h-full object-cover" alt={category.name} />
-                            </div>
-                            <div
-                              onClick={() => {
-                                getCategories(category.name);
-                              }}
-                              className="text-[20px] font-bold hover:text-primary cursor-pointer"
-                            >
-                              {category.name}
+                <div className="flex justify-center items-center">
+                  <div className="flex justify-center items-center w-full max-w-5xl">
+                    <div className="flex flex-wrap justify-center items-center gap-14">
+                      {category.map((category) => {
+                        return (
+                          <div key={`category-${category.id}`}>
+                            <div className="flex flex-col items-center gap-4 drop-shadow-2xl hover:scale-[1.1] duration-200">
+                              <div
+                                onClick={() => {
+                                  getCategories(category.name);
+                                }}
+                                className="w-[202px] h-[222px] rounded-3xl overflow-hidden relative cursor-pointer"
+                              >
+                                <img
+                                  src={category.picture}
+                                  className="w-full h-full object-cover"
+                                  alt={category.name}
+                                />
+                              </div>
+                              <div
+                                onClick={() => {
+                                  getCategories(category.name);
+                                }}
+                                className="text-[20px] font-bold hover:text-primary cursor-pointer"
+                              >
+                                {category.name}
+                              </div>
                             </div>
                           </div>
-                        </div>
-                      );
-                    })}
+                        );
+                      })}
+                    </div>
                   </div>
                 </div>
               </div>
               <div className="flex flex-col gap-8 pt-[20px]">
-                <div className="flex justify-between font-semibold">
+                <div className="flex justify-between font-semibold px-10">
                   <div>Recomended</div>
                   <div className="text-[#444cd4] cursor-pointer">More</div>
                 </div>
-                <div className="grid grid-cols-3 gap-y-12 gap-x-16">
+                <div className="flex flex-wrap justify-center items-center gap-14 px-10 py-20">
                   {article.map((article) => {
                     return (
                       <Article
@@ -209,7 +218,7 @@ const Home = () => {
               </div>
             </div>
           </div>
-          <div className="bg-[#D7DDEC] pl-[155px] pt-[135px] pb-[175px]">
+          <div className="bg-[#D7DDEC] px-32 py-32">
             <div className="flex justify-center w-full ">
               <div className="flex gap-32">
                 <div className="flex flex-col gap-8">
@@ -231,8 +240,8 @@ const Home = () => {
               </div>
             </div>
           </div>
-          <div className="bg-[#ffffff] pl-[155px] pt-[135px] pb-[175px]">
-            <div className="flex gap-16">
+          <div className="bg-[#ffffff] px-32 py-32">
+            <div className="flex w-full justify-center items-center flex-wrap gap-16">
               <div className="flex relative">
                 <img src={image} />
                 <div className="absolute -inset-x-20 -bottom-20">
@@ -260,7 +269,7 @@ const Home = () => {
           <div className="bg-white px-[60px] pt-[65px] pb-[150px]">
             <div className="flex flex-col gap-8">
               <div className="font-bold text-[24px]">Latest News</div>
-              <div className="grid grid-cols-3 gap-y-12 gap-x-16">
+              <div className="flex flex-wrap gap-14 justify-center items-center">
                 {article.map((article) => {
                   return (
                     <Article
